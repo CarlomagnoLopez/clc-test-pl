2a8e24dc57b5178a20f668d73bf8dd0dba9d12e5
Carlomagno <carlolc@Carlomagnos-MacBook-Pro-2.local>
2025-03-07 03:04:32 -0600

    added filter by nam

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,10 @@ import "./globals.css";
 export default function Home() {
   // Se requiere mostrar un catálogo de pokemones que permita buscar por nombre, además de ordenar (Alfabéticamente, ascendente y descendente), donde se mostrarán 20 tarjetas por página con un total de 4 páginas. Cada card deberá mostrar imagen, nombre y defensa/ataque. La distribución de las cards tendrá que ser de 5 filas por 4 columnas con un espacio entre ellas de 8 px, cómo se muestra a continuación.
 
-  const [pokemons, setPokemons]:any = useState()
+  const [pokemons, setPokemons]: any = useState()
   const [totalPokemons, setTotalPokemons] = useState()
   const limitToShow = 20;
+  const [value, setValue] = useState('');
   // const [imgIndex, setImgIndex] = useState(0);
 
   useEffect(() => {
@@ -23,8 +24,18 @@ export default function Home() {
       })
   }, [])
 
-  const sortAscByName = (paramPokes:any) => {
-    paramPokes.sort(function (a:any, b:any) {
+  const findPokemonByName = (paramPokes: any) => {
+    // console.log("value")
+    // console.log(value)
+    const pokemonFound = paramPokes.find((item: any) => item.name === value);
+    console.log(pokemonFound)
+    if(pokemonFound){
+      setPokemons([pokemonFound])
+    }
+  }
+
+  const sortAscByName = (paramPokes: any) => {
+    paramPokes.sort(function (a: any, b: any) {
       if (a.name < b.name) {
         return -1;
       }
@@ -36,8 +47,10 @@ export default function Home() {
     nextPokemons(1, paramPokes)
 
   }
-  const sortDescByName = (paramPokes:any) => {
-    paramPokes.sort(function (a:any, b:any) {
+
+
+  const sortDescByName = (paramPokes: any) => {
+    paramPokes.sort(function (a: any, b: any) {
       if (a.name > b.name) {
         return -1;
       }
@@ -50,14 +63,14 @@ export default function Home() {
 
   }
 
-  const nextPokemons = (page:any, paramPokes:any) => {
+  const nextPokemons = (page: any, paramPokes: any) => {
     const rangeFinalPokemons = page * limitToShow;
     const rangeStartPokemons = page * limitToShow - limitToShow;
     // setImgIndex(rangeStartPokemons)
 
-    const tempPokemons:any = [];
+    const tempPokemons: any = [];
 
-    paramPokes.forEach((element:any, index:any) => {
+    paramPokes.forEach((element: any, index: any) => {
       if (index >= rangeStartPokemons && index < rangeFinalPokemons) {
         tempPokemons.push(paramPokes[index])
         // console.log(index)
@@ -72,13 +85,20 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="grid grid-cols-4 gap-4">
+          <input
+            value={value}
+            onChange={e => { setValue(e.currentTarget.value); }}
+            type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+          <button
+            onClick={() => { findPokemonByName(totalPokemons) }}
+            type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Search</button>
+        </div>
+
         {/* <div>hi pokemon</div> */}
         <div className="grid grid-cols-4 gap-2">
-          {pokemons && pokemons.map((item:any, index:number) => {
-            // console.log(item)
+          {pokemons && pokemons.map((item: any, index: number) => {
             const imgNumber = item.url.replace('https://pokeapi.co/api/v2/pokemon/', '').replace('/', '')
-            console.log('imgNumber')
-            console.log(imgNumber)
             const urlImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/" + (imgNumber) + ".svg"
             return (<div key={index} className="cardPokemon flex items-center">
               <img src={urlImage} width={40} />
